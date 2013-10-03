19068f0f1acabad5a57be7bcd983f246af339a43
ToBeReplaced <ToBeReplaced@gmail.com>
2013-10-03 09:29:59 -0400

    Update to explicitly include clojure.data.json

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
-(defproject pedestal-content-negotiation "0.3.0"
+(defproject pedestal-content-negotiation "0.4.0-SNAPSHOT"
   :description "Content negotiation for Pedestal web services."
   :url "https://github.com/ToBeReplaced/pedestal-content-negotiation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.pedestal/pedestal.service "0.1.6"]])
+                 [io.pedestal/pedestal.service "0.2.1"]
+                 [org.clojure/data.json "0.2.3"]]
+  :global-vars {*warn-on-reflection* true}
+  :pedantic? :abort)
